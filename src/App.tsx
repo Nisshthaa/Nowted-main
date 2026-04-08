e208ad626aa0dfc3477f004e4d00c9c408fbc0d6
nisshthaa <sharmanishtha791@gmail.com>
2026-04-08 12:43:58 +0530

    refactor App component and add API client and folder API functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Layout from "./components/Layout/Layout";
-import { AppProvider } from "./context/DataProvider";
+import AppLayout from "./components/layout/AppLayout";
+import { StateProvider } from "./state/StateProvider";
 import { Toaster } from "react-hot-toast";
 
 
 
 const App: React.FC = () => {
   return (
-    <AppProvider>
-      <Layout />
+    <StateProvider>
+      <AppLayout />
       <Toaster/>
-    </AppProvider>
+    </StateProvider>
   );
 };
 
